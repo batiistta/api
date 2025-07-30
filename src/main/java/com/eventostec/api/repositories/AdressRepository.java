af0f5c11a8f218495a8eba430cd5ece3a5c82380
batiistta <gbbatisouza@gmail.com>
2025-07-30 14:46:31 -0300

    feat: add repository

---
+++ src/main/java/com/eventostec/api/repositories/AdressRepository.java
@@ -0,0 +1,6 @@
+package com.eventostec.api.repositories;
+
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+
+public interface AdressRepository extends JpaAttributeConverter{
+}
